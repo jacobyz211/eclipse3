adc221660dcc104f2e253817c3c24839be173ca4
jacobyz211 <cyrusna29@icloud.com>
2026-04-24 02:35:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,20 @@ const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 
 const DEFAULT_HIFI_INSTANCES = [
   'https://tidal-api.binimum.org',
+  'https://hifi-api-pj08.onrender.com',
+  'https://triton.squid.wtf',
   'https://ohio-1.monochrome.tf',
   'https://frankfurt-1.monochrome.tf',
+  'https://vogel.qqdl.site',
   'https://eu-central.monochrome.tf',
   'https://us-west.monochrome.tf',
   'https://hifi.geeked.wtf',
-  'https://monochrome-api.samidy.com'
+  'https://monochrome-api.samidy.com',
+  'https://hifi-two.spotisaver.net',
+  'https://wolf.qqdl.site',
+  'https://katze.qqdl.site',
+  'https://hund.qqdl.site',
+  'https://api.monochrome.tf',
 ];
 
 // ─── In-memory caches (per isolate) ─────────────────────────────────────────
